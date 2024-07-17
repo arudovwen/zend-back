fd9361d9031dd7a0a16ddc16968c32647064eeb3
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-07-17 02:01:02 +0100

    Update Announcement.tsx

--- src/components/dashboard/layouts/Announcement.tsx
+++ src/components/dashboard/layouts/Announcement.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ButtonComponent from "@/components/ButtonComponent";
 import AppIcon from "@/components/AppIcon";
+import CenterModal from "@/components/modals/CenterModal";
 
 export default function Announcement() {
+  const [isOpen, setOpen] = useState(false);
   return (
     <div>
       <div>
-        <ButtonComponent className="!bg-white dark:!bg-gray-800 !text-secondary dark:!text-white !text-sm !border !border-[#C9C8C8] dark:!border-gray-600 "><span className="flex gap-x-2 items-center"><AppIcon icon="grommet-icons:announce" /> <span className="hidden lg:inline">Announcement</span></span> </ButtonComponent>
+        <ButtonComponent
+          onClick={() => setOpen(true)}
+          className="!bg-white dark:!bg-gray-800 !text-secondary dark:!text-white !text-sm !border !border-[#C9C8C8] dark:!border-gray-600 "
+        >
+          <span className="flex gap-x-2 items-center">
+            <AppIcon icon="grommet-icons:announce" />{" "}
+            <span className="hidden lg:inline">Announcement</span>
+          </span>{" "}
+        </ButtonComponent>
       </div>
+      <CenterModal setOpen={() => {}} open={isOpen}>
+        <div className="bg-white dark:bg-gray-800 text-secondary dark:text-white p-6 rounded-lg"></div>
+      </CenterModal>
     </div>
   );
 }
