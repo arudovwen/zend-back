b21a7dd96c4e1a0fdd22b751ef68b213222f2408
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-07-23 02:42:20 +0100

    Update

--- src/components/pages/settings/team/index.tsx
+++ src/components/pages/settings/team/index.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
-
+"use client";
+import React, { useState } from "react";
+import AppSideTab from "@/components/AppSideTab";
+import AppTab from "@/components/AppTab";
+import Pending from "./pending";
+import Active from "./active";
 export default function Team() {
+  const Navigations = [
+    {
+      title: "Active",
+      key: "active",
+      sub: "List of active team members",
+    },
+    {
+      title: "Pending",
+      sub: "List of pending team requests",
+      key: "pending",
+    },
+  ];
+  const [selected, setSelected] = useState("active");
   return (
-    <div>Team</div>
-  )
+    <div className="max-w-[1024px]">
+      <div className="flex flex-col xl:flex-row gap-x-10 py-4">
+        <div className="w-auto lg:w-full sm:max-w-[230px] ">
+          <div className="hidden xl:inline-block overflow-hidden bg-white dark:bg-gray-700 rounded-lg border border-gray-100    dark:border-gray-600 w-full">
+            <AppSideTab
+              options={Navigations}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </div>
+          <div className="xl:hidden">
+            <AppTab
+              tabs={Navigations}
+              setActiveTab={setSelected}
+              activeTab={selected}
+            />
+          </div>
+        </div>
+        <div className="flex-1 py-8 px-6 sm:px-10 bg-white dark:bg-gray-700 rounded-lg border border-gray-100    dark:border-gray-600">
+          <div className="max-w-[1024px] pb-10">
+            {selected === "pending" && <Pending />}
+            {selected === "active" && <Active />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
