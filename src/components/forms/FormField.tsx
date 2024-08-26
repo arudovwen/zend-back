fb71cbcd103be1c2e166cc4e2fc586ba37ed8d86
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-08-26 17:18:48 +0100

    Update FormField.tsx

--- src/components/forms/FormField.tsx
+++ src/components/forms/FormField.tsx
@@ -110,6 +110,7 @@ const FormField: React.FC<FormFieldProps> = ({
                 {...(register ? register(name) : {})}
                 disabled={disabled}
                 type={type === "password" && isPasswordVisible ? "text" : type}
+                value={value}
                 
               />
             )}
