fc20258e456b66e1c118139c0c4fd53816a5187b
Success Ahon <successahon@gmail.com>
2024-09-05 11:56:12 +0100

    Update index.tsx

--- src/components/pages/settings/index.tsx
+++ src/components/pages/settings/index.tsx
@@ -12,7 +12,7 @@ export default function SettingsComponent() {
   const tabs = [
     { title: "Account information", key: "account", component: <Account /> },
     // { title: "Roles & Permissions", key: "roles", component: <Roles /> },
-    { title: "Devices", key: "devices", component: <Devices /> },
+    // { title: "Devices", key: "devices", component: <Devices /> },
     { title: "Security", key: "security", component: <Security /> },
     { title: "Rate settings", key: "rate", component: <Rates /> },
   ];
